1606d7e22574a6a6438a8cd1f6c266bd5550ebed
Indriyanto Nugroho <masyantoya@gmail.com>
2020-10-20 15:25:18 +0700

    initial Commit Swap Number

--- app/src/main/java/com/example/segitigabintang/Java/SwapNumber.java
+++ app/src/main/java/com/example/segitigabintang/Java/SwapNumber.java
@@ -8,13 +8,15 @@ import java.util.Scanner;
 public class SwapNumber {
     public  static void main (String[] args) {
         int x = 0;
-        int y = -10;
+        int y = 0;
         System.out.println("Nilai x asli "+ x);
         System.out.println("Nilai y asli "+ y);
 
+        System.out.print("\n");
         x = x +y ;
         y = x - y;
         x = x - y;
+
 
         System.out.println("Nilai sesudah x dibalik "+ x);
         System.out.println("Nilai sesudah y dibalik "+ y);
